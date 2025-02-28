8dac94a5cfda8dfb1be2162fa834dffea365fae9
tkusal <thiago.kusal@outlook.com>
2025-02-27 22:45:45 -0300

    Aula 27/02/2025

---
+++ Semana 1/exercicioTeste.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, resultado;
+
+    printf("Digite o primeiro número inteiro: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número inteiro: ");
+    scanf("%d", &num2);
+
+    resultado = num1 + num2;
+
+    printf("A soma dos números é igual a: %d\n", resultado);
+
+    return 0;
+}
