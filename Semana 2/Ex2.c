3a1ff15490ad33449c2cb3efe436c731dedba9b9
tkusal <thiago.kusal@outlook.com>
2025-03-11 22:44:06 -0300

    Aula 11/03/25

--- Semana 2/Ex2.c
+++ Semana 2/Ex2.c
@@ -3,3 +3,28 @@
     Como dado de entrada, o programa deverá solicitar o raio do círculo ao usuário.
     Fórmula: Área = pi x raio²
 */
+
+#include <stdio.h>
+
+#define pi 3.1416
+
+int main() {
+
+    float raio, area;
+
+    printf("Informe o raio do círculo: ");
+    scanf("%f", &raio);
+
+    area = (raio * raio) * pi;
+
+    printf("Área = %.2f", area);
+
+    /*
+        %d = inteiros decimais
+        %f = numeros float
+        %c = 1 caractere
+        %s = conjunto de caracteres (string)
+    */    
+
+    return 0;
+}
--- Semana 2/Ex3.c
+++ Semana 2/Ex3.c
@@ -2,3 +2,19 @@
     Faça um programa em C que leia uma temperatura em graus Celsius e a converta para Fahrenheit.
     Fórmula: F=(Cx9/5)+32
 */
+
+#include <stdio.h>
+
+int main() {
+
+    float celsius, fahr;
+
+    printf("Insira a temperatura em Celsius: \n");
+    scanf("%f", &celsius);
+
+    fahr = (celsius*9/5) + 32;
+
+    printf("O valor convertido de %.2fºC é de %.2fºF", celsius, fahr);
+
+    return 0;
+}
