54a7389ef8995e72caf4c7c6c1eb14f842555c6e
tkusal <thiago.kusal@outlook.com>
2025-04-15 22:27:06 -0300

    AUlas 15/04/2025

--- Semana 4/Para_Casa1.c
+++ Semana 4/Para_Casa1.c
@@ -4,3 +4,40 @@
     se ele está aprovado (media > 6) ou reprovado.
 */
 
+#include <stdio.h>
+
+#define qtdNotas 4
+
+int main()
+{
+
+    char nome[50];
+    float nota[qtdNotas], media = 0;
+    // const int qtdNotas = 4;
+
+    printf("Informe o nome do aluno: ");
+    scanf("%49[^\n]s", nome);
+
+    for (int contador = 0; contador < qtdNotas; contador++)
+    {
+        printf("Informe a nota %d: ", contador + 1);
+        scanf("%f", &nota[contador]);
+
+        media = media + nota[contador];
+        // media += nota[contador];
+    }
+
+    media /= qtdNotas;
+    // media = media / 4;
+
+    if (media > 6)
+    {
+        printf("Aluno: %s \nMédia: %.2f \nAprovado!", nome, media);
+    }
+    else
+    {
+        printf("Aluno: %s \nMédia: %.2f \nReprovado!", nome, media);
+    }
+
+    return 0;
+}
--- Semana 4/Para_Casa2 - Desafio.c
+++ Semana 4/Para_Casa2 - Desafio.c
@@ -1,5 +1,37 @@
 /*
     Escreva um programa que leia a idade de 5 pessoas e armazene em um vetor.
     O programa deverá imprimir na tela essas cinco idades em ordem crescente.
+
+    //Bubble Sort
 */
 
+#include <stdio.h>
+
+#define qtdIdades 5
+
+int main() {
+    int idade[qtdIdades];
+    int box;
+
+    for (int x = 0; x < qtdIdades; x++) {
+        printf("Escreva a idade %d: ", x+1);
+        scanf("%d", &idade[x]);
+    }
+
+    //Selection Sort
+    for (int x = 0; x < qtdIdades-1; x++) {
+        for (int z = x+1; z < qtdIdades; z++) {
+            if (idade[x] > idade [z]) {
+                box = idade[x];
+                idade[x] = idade[z];
+                idade[z] = box;
+            }
+        }
+    }
+
+    for (int x = 0; x < qtdIdades; x++) {
+        printf("%d - ", idade[x]);
+    }
+
+    return 0;
+}
